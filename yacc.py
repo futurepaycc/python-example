c64359a1398282e968ad0cedc0f84e0572fed2a6
147403 <liuhui@syswin.com>
2017-07-03 14:50:41 +0800

    modity yacc.py,and first run

--- yacc.py
+++ yacc.py
@@ -1,29 +1,39 @@
-#coding:utf-8
+# coding:utf-8
 
 # 还有点小错误，必须加以改正
-tokens=(
-    "NOUN","PRONOUN","VERB"
+tokens = (
+    "NOUN", "PRONOUN", "VERB"
 )
 
-def t_VERB(t):
-    r"like|help|fuck"
-    print(t.value + " is a verb")
+t_NOUN = r"dog|man|woman"
+t_PRONOUN = r"i|he|she|item"
+t_VERB = r"like|help|fuck"
 
-def t_NOUN(t):
-    r"dog|man|woman"
-    print(t.value + " is a noun")
+# 问题出现在下面这些函数上!!!!!!!
+# def t_VERB(t):
+#     r"like|help|fuck"
+#     print(t.value + " is a verb")
 
-def t_PRONOUN(t):
-    r"i|he|she|item"
-    print(t.value + " is a pron")
+
+# def t_NOUN(t):
+#     r"dog|man|woman"
+#     print(t.value + " is a noun")
+
+
+# def t_PRONOUN(t):
+#     r"i|he|she|item"
+#     print(t.value + " is a pron")
+
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
 
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
 
 t_ignore = ' \t'
 
@@ -31,34 +41,43 @@ import ply.lex as lex
 lexer = lex.lex()
 
 
-################句法部分#####################3
-def p_statement_sentence(p):
+# 句法部分#####################3
+def p_statement_expr(p):
     'statement : expression'
-    print(p[1])
+    print(p[1]+" is a valid sentence")
+
 
 def p_expression_binop(p):
-    '''expression : expression VERB expression'''
-    p[0] = p[1] + p[2]+p[3]
+    'expression : expression VERB expression'
+    p[0] = p[1] + p[2] + p[3]
 
 # 基本表达式 名词
+
+
 def p_expression_noun(p):
     'expression : NOUN'
     p[0] = p[1]
 
 # 基本表达式 名词
+
+
 def p_expression_pronoun(p):
     'expression : PRONOUN'
-    p[0] = p[1]    
+    p[0] = p[1]
+
 
 def p_error(p):
     print("Syntax error at '%s'" % p)
 
+
 import ply.yacc as yacc
-parser = yacc.yacc()
+# parser = yacc.yacc()
+yacc.yacc()
 
 while True:
     try:
         s = input('yacc1 > ')
-        parser.parse(s)
+        # parser.parse(s)
+        yacc.parse(s)
     except EOFError:
         break
