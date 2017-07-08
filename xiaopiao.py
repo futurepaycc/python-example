93589e435468eadd4e0827bf2bfabe8cd4436f16
liunix <liuyifan.com@gmail.com>
2017-07-08 14:43:16 +0800

    add regex parser

--- xiaopiao.py
+++ xiaopiao.py
@@ -2,9 +2,15 @@
 
 #按行和列的思维分析小票
 #统一给出结论
+import re
+
+parttern="(\w+)\s+(\d+)\s+(\d)+\s+(\d+\.\d{2})"
 
 file = open("xiaopiao1.txt")
 content = file.read()
-print(content)
+# print(content)
 result = content.split('\n')
-print(result[0])
+for line in result:
+    m = re.match(parttern,line)
+    if m is not None:
+        print(m.groups()) #m.group(1) m.group(2) m.group(3) m.group(4)
