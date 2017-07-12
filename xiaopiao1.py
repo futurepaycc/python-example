de258b77fab19eb2bed63372193098e6008119e4
liunix <liuyifan.com@gmail.com>
2017-07-12 23:30:35 +0800

    clean and add comment

--- xiaopiao1.py
+++ xiaopiao1.py
@@ -1,19 +1,6 @@
 #coding:utf-8
 
-#按行和列的思维分析小票
-#统一给出结论
-# import re
-
-# parttern="(\w+)\s+(\d+)\s+(\d)+\s+(\d+\.\d{2})"
-
-# file = open("xiaopiao1.txt")
-# content = file.read()
-# # print(content)
-# result = content.split('\n')
-# for line in result:
-#     m = re.match(parttern,line)
-#     if m is not None:
-#         print(m.groups()) #m.group(1) m.group(2) m.group(3) m.group(4)
+# TODO:正则表达式抽取处理,主内容区有多个分组
 
 import sys
 #获取当前模块对像，用于反射
