9361340f9d15adb415214b9f70ae63e79085fb08
liunix <liuyifan.com@gmail.com>
2017-07-12 23:27:21 +0800

    add reflect invoke method

--- xiaopiao1.py
+++ xiaopiao1.py
@@ -15,18 +15,29 @@
 #     if m is not None:
 #         print(m.groups()) #m.group(1) m.group(2) m.group(3) m.group(4)
 
+import sys
+#获取当前模块对像，用于反射
+thismodule = sys.modules[__name__]
+
 templateContent = open("xiaopiao1.pattern").read()
 exampleContent = open("xiaopiao1.txt").read()
 
 templateLines = templateContent.split('\n')
 exampleLines = exampleContent.split('\n')
 
+def hello_merchantName(name):
+    print("hello: %s"%name)
+
+def hello_orderTime(name):
+    print("hello: %s"%name)
+    
 
 def parseTemplateLine(templateLine):
     name,line,parttern = templateLine.split(',')
-    print(name)
-
-
+    #用反射获取方法并执行
+    if(name == "merchantName" or name == "orderTime"):
+        func=getattr(thismodule,"hello_"+name)
+        func(name)
 
 for templateLine in templateLines:
     if(len(templateLine.strip()) == 0 or templateLine.find('#') == 0):
