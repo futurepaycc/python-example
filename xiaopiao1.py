9111d9aeee2a58d1c383980204911fa1af1df1c0
liunix <liuyifan.com@gmail.com>
2017-07-13 09:24:00 +0800

    first regexp extract

--- xiaopiao1.py
+++ xiaopiao1.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 
 # TODO:正则表达式抽取处理,主内容区有多个分组
-import sys
+import sys,re
 #获取当前模块对像，用于反射
 thismodule = sys.modules[__name__]
 
@@ -13,13 +13,17 @@ exampleLines = exampleContent.split('\n')
 
 def hello_merchantName(name,lineNo,parttern):
     print("hello: %s %s %s"%(name,lineNo,parttern))
+    no = lineNo.replace('L','')
+    content = exampleLines[int(no)]
+    m = re.match(parttern.strip(),content.strip())
+    # print(m.group())
+    print("商户名为：%s"%m.groups(1))
 
 def hello_orderTime(name,lineNo,parttern):
     print("hello: %s %s %s"%(name,lineNo,parttern))
     
-
 def parseTemplateLine(templateLine):
-    name,lineNo,parttern = templateLine.split(',')
+    name,lineNo,parttern = templateLine.split(',') #得到的结果没有strip
     #用反射获取方法并执行
     if(name == "merchantName" or name == "orderTime"):
         func=getattr(thismodule,"hello_"+name)
