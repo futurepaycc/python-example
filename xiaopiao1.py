ffc544bef689402933c487ed7fd5db9bd38ed164
liunix <liuyifan.com@gmail.com>
2017-07-13 08:50:03 +0800

    print line part

--- xiaopiao1.py
+++ xiaopiao1.py
@@ -1,7 +1,6 @@
 #coding:utf-8
 
 # TODO:正则表达式抽取处理,主内容区有多个分组
-
 import sys
 #获取当前模块对像，用于反射
 thismodule = sys.modules[__name__]
@@ -12,24 +11,22 @@ exampleContent = open("xiaopiao1.txt").read()
 templateLines = templateContent.split('\n')
 exampleLines = exampleContent.split('\n')
 
-def hello_merchantName(name):
-    print("hello: %s"%name)
+def hello_merchantName(name,lineNo,parttern):
+    print("hello: %s %s %s"%(name,lineNo,parttern))
 
-def hello_orderTime(name):
-    print("hello: %s"%name)
+def hello_orderTime(name,lineNo,parttern):
+    print("hello: %s %s %s"%(name,lineNo,parttern))
     
 
 def parseTemplateLine(templateLine):
-    name,line,parttern = templateLine.split(',')
+    name,lineNo,parttern = templateLine.split(',')
     #用反射获取方法并执行
     if(name == "merchantName" or name == "orderTime"):
         func=getattr(thismodule,"hello_"+name)
-        func(name)
+        func(name,lineNo,parttern)
 
 for templateLine in templateLines:
     if(len(templateLine.strip()) == 0 or templateLine.find('#') == 0):
         continue
     else:
         parseTemplateLine(templateLine)
-
-
